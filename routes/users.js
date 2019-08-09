523eb5cc2567a880796f69df3388e3d0235f75c5
Rose <hey_cool@163.com>
2019-08-09 18:31:27 +0800

    调通了接口

--- routes/users.js
+++ routes/users.js
@@ -2,35 +2,22 @@ const router = require('koa-router')()
 const db = require("../config/db");
 router.prefix('/users')
 
-router
-  .param('users', (page, pagesize,ctx, next) => {
-  console.log(page, pagesize)
-  
-    ctx.user = users[id];
-    if (!ctx.user) return ctx.status = 404;
-    return next();
-  })
-  
+router.post('/', async (ctx, next) => {
 
-router.all('users/:page/:pagesize', async (ctx, next) => {
-console.log(ctx)
+    let page =  ctx.request.body.page;
+    let pagesize =  ctx.request.body.pagesize;
 
-    let page =  ctx.request.body.page;   
-    
-    let pagesize =  ctx.request.body.pagesize£»    
-    let count = ( page-1 )* pagesize;
-
-
-  var sql = 'SELECT * FROM v9_news ORDER BY id desc limit ' + count + ','+ pagesize;
-  var tmp = await db.query(sql).then(function(result) {
-    console.log(result);
-    return result;
-  }, function(error){
-    return -1;
-  });
-   ctx.body = {
-   data: tmp,
-   flag: true
-   };
+    let count = ( page - 1 )* pagesize;
+    var sql = 'SELECT * FROM v9_news ORDER BY id desc limit ' + count + ','+ pagesize;
+    var tmp = await db.query(sql).then(function(result) {
+        return result;
+    }, function(error){
+        return -1;
+    });
+    ctx.body = {
+        data: tmp,
+        flag: true
+    };
 })
+
 module.exports = router
