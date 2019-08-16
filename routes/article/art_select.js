229fa66d17eacad31871030b52b912f42a238849
Rose <hey_cool@163.com>
2019-08-16 18:32:19 +0800

    修改文章sql

--- routes/article/art_select.js
+++ routes/article/art_select.js
@@ -9,7 +9,9 @@ router.post('/', async(ctx, next) => {
     if (id) {
         var sql = 'SELECT v9_news.id, v9_news.catid, v9_news.description, v9_news.keywords, v9_news.thumb, v9_news.title, v9_news_data.content FROM v9_news INNER JOIN v9_news_data ON v9_news.id = v9_news_data.id WHERE v9_news.id = ' + id
     } else {
-        var sql = 'SELECT * FROM v9_news ORDER BY id desc limit ' + count + ',' + pagesize;
+        // var sql = 'SELECT * FROM v9_news ORDER BY id desc limit ' + count + ',' + pagesize;
+
+        var sql = "SELECT hits.views,news.* FROM (SELECT SUBSTRING_INDEX(hitsid, '-', -1) AS hid,views FROM v9_hits) AS hits LEFT JOIN v9_news AS news ON hits.hid=news.id ORDER BY id desc limit " + count + "," + pagesize;
     }
     console.log(sql)
     var tmp = await db.query(sql).then(function(result) {
