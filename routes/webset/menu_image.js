0e9cacbd5c7d3df2c8c90e925867ff5a794accd6
Rose <hey_cool@163.com>
2019-08-23 18:38:18 +0800

    添加上一张 下一章的接口

--- routes/webset/menu_image.js
+++ routes/webset/menu_image.js
@@ -7,14 +7,27 @@ router.prefix('/menu_image')
 /**
  * search
  */
-router.post('/', async(ctx, next) => {
+router.post('/', async (ctx, next) => {
 
-    let page =  ctx.request.body.page;
-    let pagesize =  ctx.request.body.pagesize;
+    let page = ctx.request.body.page;
+    let pagesize = ctx.request.body.pagesize;
+    let count = (page - 1) * pagesize;
 
-    let count = ( page - 1 )* pagesize;
+    let aid = ctx.request.body.aid;
+    let prevone = ctx.request.body.prevone;
+    let nextone = ctx.request.body.nextone;
 
-    var sql = 'SELECT * FROM v9_attachment ORDER BY aid DESC LIMIT ' + count + ','+ pagesize;
+    if (aid && prevone) {
+        var sql = 'SELECT * FROM v9_attachment AS n WHERE n.aid < '+aid +' ORDER BY n.aid DESC LIMIT 0,1';
+
+    } else if (aid && nextone) {
+        var sql = 'SELECT * FROM v9_attachment AS n WHERE n.aid > '+aid +' ORDER BY n.aid LIMIT 0,1';
+
+    } else {
+        var sql = 'SELECT * FROM v9_attachment ORDER BY aid DESC LIMIT ' + count + ',' + pagesize;
+    }
+
+
 
     console.log(sql)
 
@@ -26,7 +39,7 @@ router.post('/', async(ctx, next) => {
     });
     ctx.body = {
         data: tmp,
-        flag: tmp == -1 ? false: true
+        flag: tmp == -1 ? false : true
     };
 });
 module.exports = router; //不加这句会报错： Router.use() requires a middleware function but got a Object （没有向外暴露，导致app.use引用不到）
